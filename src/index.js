825bdf8c50667e357972ca6d6f971b400506445d
Pedro <windowsxpedro@gmail.com>
2020-08-20 17:53:00 -0300

    added ApprovalCard

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Comment from './components/comment.js'
+import Comment from './components/Comment.js'
+import ApprovalCard from './components/ApprovalCard.js'
 
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <Comment profile="Sam" text="This is a very nice blog!" time="18:00"/>
+            <ApprovalCard>
+                <Comment profile="Sam" text="This is a very nice blog!" time="18:00"/>
+            </ApprovalCard>
         </div>
     )
 }
